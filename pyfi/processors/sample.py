3fdb5faa9858aa95e3ae96eeb69ba71c790e71ea
Darren Govoni <darren@rapidquest.ai>
2021-07-30 19:20:37 -0400

    Update

--- pyfi/processors/sample.py
+++ pyfi/processors/sample.py
@@ -5,3 +5,7 @@ def do_something(message):
    print("DO SOMETHING ELSE!")
    print("Message:",message)
    return "Message:",message
+
+def do_this(message):
+   print("Do this!",message)
+   return "Do this: "+message
