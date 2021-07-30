27646e0689fa6e6bc4ec269a30c85ac262ec5931
Darren Govoni <darren@rapidquest.ai>
2021-07-30 19:41:38 -0400

    Update

--- pyfi/processors/sample.py
+++ pyfi/processors/sample.py
@@ -4,7 +4,7 @@ from celery import Celery
 def do_something(message,outlets=[]):
    print("DO SOMETHING ELSE!")
    print("Message:",message)
-   outlets['queue3'] += ["Message "+message]
+   outlets['pyfi.queue3'] += ["Message "+message]
    return "Message:",message
 
 def do_this(message,outlets=[]):
