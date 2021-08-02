3016563590550bed90e339f7e39113c3c84117c4
Darren Govoni <darren@rapidquest.ai>
2021-08-02 17:58:11 -0400

    Update

--- pyfi/processors/sample.py
+++ pyfi/processors/sample.py
@@ -1,10 +1,12 @@
 
-def do_something(message, plugs={}):
+def do_something(message, plugs={}, **kwargs):
    print("PLUGS:",plugs)
    print("DO SOMETHING ELSE!")
    print("Message:",message)
    if 'pyfi.queue2' in plugs: 
       plugs['pyfi.queue2'] = ["Message "+message]
+   kwargs['result'] = "The result!"
+   kwargs['data'] = {'key':'result'}
    return "Message:",message
 
 def do_this(message,plugs=[]):
