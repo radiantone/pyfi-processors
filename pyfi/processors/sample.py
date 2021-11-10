4d6d01c440b2219c2e8e240061a185f88ff5bffc
Darren Govoni <darren@rapidquest.ai>
2021-11-10 05:24:31 -0500

    String change

--- pyfi/processors/sample.py
+++ pyfi/processors/sample.py
@@ -10,13 +10,13 @@ def do_something(message, *args, plugs={}, output={}, **kwargs):
    output['data'] = {'key':'result'}
 
    argstr = ' '.join(args)
-   return "Message:"+str(message)+argstr
+   return "TEXT:"+str(message)+argstr
 
 def do_this(message, *args, plugs={}, output={},**kwargs):
    print("Do this!", message)
 
    argstr = ' '.join(args)
-   return "Do this: "+str(message)+argstr
+   return "Do this String: "+str(message)+argstr
 
 def do_that(message, *args, plugs={}, output={},**kwargs):
    print("Do THAT!", message)
